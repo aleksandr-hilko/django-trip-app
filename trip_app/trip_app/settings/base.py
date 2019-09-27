7da19f64e5ab363fef355fde746b9f834f4dbf94
alexander.khilko <alexkhilko92@gmail.com>
2019-09-27 15:16:47 +0300

    add postgresql db backend to local settings

--- trip_app/trip_app/settings/base.py
+++ trip_app/trip_app/settings/base.py
@@ -9,12 +9,21 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
 import os
+from django.core.exceptions import ImproperlyConfigured
+
+
+def get_env_variable(var_name):
+    """Get the environment variable or return exception."""
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(var_name)
+    raise ImproperlyConfigured(error_msg)
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -26,7 +35,6 @@ SECRET_KEY = 'gdgh$)yv7v8*wuiw^0hk5l(9e57&=b1giij#5hq6=$5732y=95'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,7 +78,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'trip_app.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -80,7 +87,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -100,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -113,7 +118,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
--- trip_app/trip_app/settings/local.py
+++ trip_app/trip_app/settings/local.py
@@ -5,3 +5,14 @@ INSTALLED_APPS += ("debug_toolbar",)
 MIDDLEWARE += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
 
 INTERNAL_IPS = ('127.0.0.1',)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+        'USER': get_env_variable("POSTGRES_USER"),
+        'PASSWORD': get_env_variable("POSTGRES_PASSWORD"),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
