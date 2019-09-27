3ccd9606d33689abacec7207a3d9fcef2e1756f4
alexander.khilko <alexkhilko92@gmail.com>
2019-09-27 17:44:34 +0300

    add docker and docker-compose files

--- trip_app/trip_app/settings/local.py
+++ trip_app/trip_app/settings/local.py
@@ -1,11 +1,5 @@
 from .base import *
 
-INSTALLED_APPS += ("debug_toolbar",)
-
-MIDDLEWARE += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
-
-INTERNAL_IPS = ('127.0.0.1',)
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
