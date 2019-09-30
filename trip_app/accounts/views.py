64e4642ba740d78658689f9d8f9bad32e330fc7f
alexander.khilko <alexkhilko92@gmail.com>
2019-09-30 13:55:14 +0300

    add users/ endpoints

--- trip_app/accounts/views.py
+++ trip_app/accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
-# Create your views here.
+from .serializers import UserSerializer
+
+User = get_user_model()
+
+
+class UserList(ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
